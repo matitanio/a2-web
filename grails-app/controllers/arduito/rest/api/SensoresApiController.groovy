cb3003470e5e3b9aa7cdb2157fe7eebdc9584e32
Matias Gonzalez <mgon.uat@gmail.com>
2013-09-05 12:01:19 -0300

    primer version de la api rest

--- grails-app/controllers/arduito/rest/api/SensoresApiController.groovy
+++ grails-app/controllers/arduito/rest/api/SensoresApiController.groovy
@@ -1,5 +1,18 @@
 package arduito.rest.api
 
+import grails.converters.JSON
+import arduito.RegistroSensor
+import arduito.SensorHabitacion
+
 class SensoresApiController {
 
+	
+	
+	def sensor(Long id){
+		
+		def sensor = SensorHabitacion.get(id)
+		def lista = RegistroSensor.findAllBySensor(sensor)
+		
+		render lista as JSON
+	}
 }
