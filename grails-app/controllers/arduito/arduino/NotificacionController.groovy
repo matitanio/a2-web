710839623580190578758351bcf0f323e834d427
Matias Gonzalez <mgon.uat@gmail.com>
2013-09-11 17:34:57 -0300

    se modifica el controller de notificacion para poder desplegar momentanemente en heroku

--- grails-app/controllers/arduito/arduino/NotificacionController.groovy
+++ grails-app/controllers/arduito/arduino/NotificacionController.groovy
@@ -1,15 +1,42 @@
 package arduito.arduino
 
+import arduito.Habitacion
+import arduito.Sensor
+import arduito.SensorHabitacion
+
 /**
  * NotificacionController
  * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
  */
 class NotificacionController {
 
+	def registroService
+	
 	def index = {
 		
 		
-		sendJMSMessage("arduino.queue", params)
+		//sendJMSMessage("arduino.queue", params)
+		onMessage(params)
 		render 'ok'
+	}
+	
+	void onMessage(msg){
+		
+		//sensorService.sensorActivado(msg.sensor)
+		
+		//identificar el sensor
+		def sensor = buscarSensor(msg)
+		registroService.registrarMedicion(sensor, new Date(), msg.valores)
+		//validar
+		
+	}
+	
+	private buscarSensor(msg){
+		
+		def habitacion = Habitacion.get(msg.habitacion.toLong())
+		def sensor = Sensor.get(msg.sensor.toLong())
+		
+		SensorHabitacion.findByHabitacionAndSensorAndNumeroSensor(habitacion,sensor,msg.numeroSensor)
+		
 	}
 }
